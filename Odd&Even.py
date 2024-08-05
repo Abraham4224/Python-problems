430e0727522f5647cd958bae42d1c42925a71c1d
Abraham4224 <130403977+Abraham4224@users.noreply.github.com>
2024-08-05 10:29:02 -0600

    File created for discovering if a number is odd or even

---
+++ Odd&Even.py
@@ -0,0 +1,13 @@
+def isOdd(number):
+    if number%2!=0:
+        return True
+    else:
+        return False
+
+def isEven(number):
+    if number%2==0:
+        return True
+    else:
+        return False
+print(isOdd(42))  
+print(isEven(42))
