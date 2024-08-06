4a73ea09f57c45daa9de9b1e1a756214015fdd4a
Abraham4224 <130403977+Abraham4224@users.noreply.github.com>
2024-08-06 16:35:33 -0600

    Shows how to read and write and example file

---
+++ Write-File.py
@@ -0,0 +1,16 @@
+def writeToFile(text,code):
+    with open(text,'w') as Txt:
+        Txt.write(code)
+    
+def appendToFile(text,code):
+    with open(text,'a') as App:
+        App.write(code)
+        
+def readFromFile(text):
+    with open(text) as leer:
+        ok=leer.read()
+    return ok
+
+writeToFile('greet.txt','Hello!\n')
+appendToFile('greet.txt','Goodbye!\n')
+print(readFromFile('greet.txt'))
