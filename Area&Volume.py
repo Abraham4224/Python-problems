a66f8cd29d73420a618d8fe2c6e28d8ff6a23b72
Abraham4224 <130403977+Abraham4224@users.noreply.github.com>
2024-08-06 16:29:32 -0600

    Updated

---
+++ Area&Volume.py
@@ -0,0 +1,11 @@
+def area(length,width):
+    return length*width
+
+def perimeter(length,width):
+    return (2*length)+(2*width)
+
+def volume(length,width,height):
+    return length*width*height
+
+def surfaceArea(length,width,height):
+    return (length*width*2)+(length*height*2)+(width*height*2)
