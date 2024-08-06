1273a22d7451cdbebe2ffc17b65e032309f017a4
Abraham4224 <130403977+Abraham4224@users.noreply.github.com>
2024-08-06 16:36:21 -0600

    Back to normal code

--- Hello-World.py
+++ Hello-World.py
@@ -7,20 +7,5 @@ print("Hello, "+ name)
 
   
 
-def writeToFile(text,code):
-    with open(text,'w') as Txt:
-        Txt.write(code)
-    
-def appendToFile(text,code):
-    with open(text,'a') as App:
-        App.write(code)
-        
-def readFromFile(text):
-    with open(text) as leer:
-        ok=leer.read()
-    return ok
 
-writeToFile('greet.txt','Hello!\n')
-appendToFile('greet.txt','Goodbye!\n')
-print(readFromFile('greet.txt'))
 
