fad3e11f38860bc1bba0de582a777dc48d775caf
Abraham4224 <130403977+Abraham4224@users.noreply.github.com>
2024-08-06 16:31:12 -0600

    It labels the correct ordinal suffix to the number

---
+++ Ordinal-Suffix.py
@@ -0,0 +1,14 @@
+def ordinalSuffix(number):
+    num=str(number)
+    if num.endswith("0") and len(num)==1:
+        print(num+"th")
+    elif num.endswith("1") and len(num)==1:
+        print(num+"st")
+    elif num.endswith("2") and len(num)==1:
+        print(num+"nd")
+    elif num.endswith("3") and len(num)==1:
+        print(num+"rd")
+    elif (number>=4 and number<=99): 
+        print(num+"th")
+    elif  number >=100:
+        print(num+"st")
