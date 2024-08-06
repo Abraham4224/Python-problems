f203a3c8525971078a6a71f7915faf93ef143e03
Abraham4224 <130403977+Abraham4224@users.noreply.github.com>
2024-08-06 16:32:23 -0600

    It stores new code

--- Hello-World.py
+++ Hello-World.py
@@ -5,18 +5,6 @@ name = input()
 print("Hello, "+ name)
 
 
-
-def fizzBuzz(upTo):
-    for i in range(1,upTo+1):
-            if i%3==0 and i%5==0:
-                print( "FizzBuzz",end=" ")
-            elif i%3==0:
-                print( "Fizz", end=" ")
-            elif i%5==0:
-                print( "Buzz",end=" ")
-            else:
-                print(i, end=" ")
-
   
 
 def writeToFile(text,code):
@@ -36,17 +24,3 @@ writeToFile('greet.txt','Hello!\n')
 appendToFile('greet.txt','Goodbye!\n')
 print(readFromFile('greet.txt'))
 
-def ordinalSuffix(number):
-    num=str(number)
-    if num.endswith("0") and len(num)==1:
-        print(num+"th")
-    elif num.endswith("1") and len(num)==1:
-        print(num+"st")
-    elif num.endswith("2") and len(num)==1:
-        print(num+"nd")
-    elif num.endswith("3") and len(num)==1:
-        print(num+"rd")
-    elif (number>=4 and number<=99): 
-        print(num+"th")
-    elif  number >=100:
-        print(num+"st")
