02b801b1819feb07248090a1361a471a96b96394
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-08 14:04:56 -0600

    Create Hours-minutes-seconds.py

---
+++ Hours-minutes-seconds.py
@@ -0,0 +1,26 @@
+def getHoursMinutesSeconds(totalSeconds):
+    if totalSeconds == 0: return '0s'
+    horas=0
+    minutos=0
+    segundos=0
+    while totalSeconds>=3600:
+        totalSeconds-=3600
+        horas+=1
+        
+    while totalSeconds>=60:
+        minutos+=1
+        totalSeconds-=60
+        
+    segundos=totalSeconds
+    
+    lista=[]
+    if horas>0:
+        lista.append(str(horas)+'h')
+    if minutos>0:
+        lista.append(str(minutos)+'m')
+    if segundos>0:
+        lista.append(str(segundos)+'s')
+        
+    return ' '.join(lista)
+        
+print(getHoursMinutesSeconds(3661))
