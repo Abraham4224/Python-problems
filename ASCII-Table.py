29c67214f40194fcf6e8a8d00df86577332110b0
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-07 20:32:40 -0600

    Update ASCII-Table.py

--- ASCII-Table.py
+++ ASCII-Table.py
@@ -1,4 +1,4 @@
-#Prints out from the number 32 to 127 from the ASCII Table
+#Prints out the characters from the number 32 to 127 from the ASCII Table
 def printASCIITable():
     for i in range(32,127):
         print(i,chr(i))
