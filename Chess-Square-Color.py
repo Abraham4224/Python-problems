a7122065cd1041f0af0ec3ed2379cd54e1fbaa73
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-09 14:14:03 -0600

    Update Chess-Square-Color.py

--- Chess-Square-Color.py
+++ Chess-Square-Color.py
@@ -1,3 +1,6 @@
+print("There are 8x8 squares on a chess board, they can be white and black, this code return the color of the square given")
+x=int(input("Enter the number of the x-axis:"))
+y=int(input("Enter the number of the y-axis:"))
 def getChessSquareColor(x,y):
     if x>=8 or y>=8:
         return ''
@@ -9,7 +12,7 @@ def getChessSquareColor(x,y):
         return "black"
     elif x%2!=0 and y%2!=0:
         return "white"
-print(getChessSquareColor(4,8))    
+print(getChessSquareColor(x,y))    
 #This is another way to do it
 def ChessSquareColor(x,y):
     if x>=8 or y>=8:
