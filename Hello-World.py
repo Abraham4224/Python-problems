89147efb3ae65f654705b96382dd388fc18c6f9e
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-11 13:32:16 -0600

    Update Hello-World.py

--- Hello-World.py
+++ Hello-World.py
@@ -1,4 +1,4 @@
-#The
+#The first code of almost every programmer
 print("This is the easiest and the very first problem that every programmer learns")
 print("Hello, World!")
 print("What is your name?")
