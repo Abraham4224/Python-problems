2ce1baa48ba1ef17615776d97806f47d99cf683d
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-12 09:35:48 -0600

    Create Average.py

---
+++ Average.py
@@ -0,0 +1,8 @@
+def average(numbers):
+    if len(numbers)==0: return None
+    contador=0
+    for u in numbers:
+        contador=contador+u
+    return contador/len(numbers)
+
+print(average([12,20,37]))
