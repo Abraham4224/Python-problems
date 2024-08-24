446cb533cd7ca80680b96a0930b2a63569900a27
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-08-24 12:23:22 -0600

    Create max-prime-numbers.py

---
+++ max-prime-numbers.py
@@ -0,0 +1,14 @@
+def primeNumbers(n):
+    i=2
+    factors=[]
+    while n>=i*i:
+        if  n % i!=0:
+            i+=1
+        else:
+            n=n//i
+            factors.append(i)
+    if n>1:
+        factors.append(n)
+    return max(factors)
+
+print(primeNumbers(138))
