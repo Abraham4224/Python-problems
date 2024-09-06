c93f349c5a58e67ae9a0d4a4c51a8fe44f45ac85
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-09-06 13:45:26 -0600

    Update m-box-short.py

--- m-box-short.py
+++ m-box-short.py
@@ -1,1 +1,15 @@
-
+name=input("Enter the name of the file: ")
+if len(name)<=0: 
+    name = "mbox-short.txt"
+file=open(name)  
+suma=0
+contador=0
+for line in file:
+    line1=line.rstrip()
+    if line1.startswith("X-DSPAM-Confidence:"):
+        found=line1.find(" ")
+        sliced=line1[found+1:]
+        #print(sliced)
+        suma+=float(sliced)
+        contador+=1
+print("Promedio spam confidence: %f" % (suma/contador)) 
