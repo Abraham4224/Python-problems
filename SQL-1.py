6cfa175f143d1bc8741f0751cc58f44d2de93d2c
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-10-13 12:42:10 -0600

    Update SQL-1.py

--- SQL-1.py
+++ SQL-1.py
@@ -1,6 +1,6 @@
 import sqlite3
- conn = sqlite3.connect(musica.sqlite)
- cur = conn.cursor()
- cur.execute(DROP TABLE IF EXISTS Canciones)
- cur.execute(CREATE TABLE Canciones (titulo TEXT, reproducciones INTEGER))
- conn.close()
+conn = sqlite3.connect(musica.sqlite)
+cur = conn.cursor()
+cur.execute(DROP TABLE IF EXISTS Canciones)
+cur.execute(CREATE TABLE Canciones (titulo TEXT, reproducciones INTEGER))
+conn.close()
