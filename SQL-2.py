a1b14795fd03c5638b03ad7bc172800b69de5509
Abraham <130403977+Abraham4224@users.noreply.github.com>
2024-10-14 09:59:45 -0600

    Create SQL-2.py

---
+++ SQL-2.py
@@ -0,0 +1,14 @@
+import sqlite3
+ conn = sqlite3.connect(musica.sqlite)
+ cur = conn.cursor()
+ cur.execute(INSERT INTO Canciones (titulo, reproducciones) VALUES (?, ?),
+ (Thunderstruck, 20))
+ cur.execute(INSERT INTO Canciones (titulo, reproducciones) VALUES (?, ?),
+ (My Way, 15))
+ conn.commit()
+ print(Canciones:)
+ cur.execute(SELECT titulo, reproducciones FROM Canciones)
+ for fila in cur:
+ print(fila)
+ cur.execute(DELETE FROM Canciones WHERE reproducciones < 100)
+ conn.commit()
